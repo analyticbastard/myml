c04abfa3667804ea7d7cbbfae08a4a40155ebda8
buhos_000 <albrecht.allenstein@gmail.com>
2013-10-31 18:16:51 +0100

    Fix to KMeans

--- myml/unsupervised/kmeans.py
+++ myml/unsupervised/kmeans.py
@@ -14,43 +14,41 @@ import base
 
 class KMeans(base.AbstractUnsupervisedMethod):
     
-    def __init__(self, k = 2, maxiter = 100):
-        self.k_ = k
+    def __init__(self, n_components = 2, maxiter = 100):
+        self.n_components_ = n_components
         self.maxiter_ = maxiter
     
     
     def fit(self, X):
         n, self.m_ = X.shape
         
-        indices = np.random.randint(0, n, self.k_)
+        indices = np.random.randint(0, n, self.n_components_)
         
-        centr = X[:,indices]
+        centr = X[indices,:]
         
         # One column for distances, another for centroid index
         assig = np.zeros((n,2))
         assig[:,0] = sys.maxint
         
         for it in range(self.maxiter_):
-            assig = self.maximization(X, centr, assig)
-            centr = self.expectation(X, centr, assig)
+            self.maximization(X, centr, assig)
+            self.expectation(X, centr, assig)
         
         self.centr_ = centr
          
             
     
     def maximization(self,X,centr,assig):
-        for j in range(self.k_):
-            dist = np.sum((X-centr[j,:]).abs(), axis=1)
+        for j in range(self.n_components_):
+            dist = np.sum(np.abs(X-centr[j,:]), axis=1)
             ind  = dist<assig[:,0]
             assig[ind,0] = dist[ind]
             assig[ind,1] = j
-            
-        return assig
     
     
     def expectation(self,X,centr,assig):
-        for j in range(self.k_):
-            ind = assig[:,1] == j
+        for j in range(self.n_components_):
+            ind = (assig[:,1] == j)
             centr[j, :] = np.mean(X[ind,:], axis=0)
     
     
